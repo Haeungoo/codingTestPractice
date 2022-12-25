1db1c7a2f1d3b67e8f86197f9717186738e48e29
Haeun Goo <becauseiami98@gmail.com>
2022-12-26 00:10:01 +0900

    Programmers (코딩테스트 연습 > 코딩테스트 입문 > 분수의 덧셈)

---
+++ src/main/java/org/example/Programmers_Lv0_2.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Programmers_Lv0_2 {
+    static int denum1 = 1;
+    static int num1 = 2;
+    static int denum2 =3;
+    static int num2 = 4;
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(function(denum1, num1, denum2, num2)));
+
+    }
+    private static int[] function(int denum1, int num1, int denum2, int num2){
+        int[] answer = new int[2];
+        int max = 1;
+        int num = num1 * num2;
+        int de = denum1 * num2 + denum2 * num1;
+        for(int i = 1 ; i <= num && i <= de; i++){
+            if(num%i == 0 && de%i == 0){
+                max = i;
+            }
+        }
+        answer[0] = de/max;
+        answer[1] = num/max;
+
+        return answer;
+    }
+}
