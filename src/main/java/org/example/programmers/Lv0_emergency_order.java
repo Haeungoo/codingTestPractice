083dbecac7bc3b506e39e7dbc8a3680b2e8ea229
Haeun Goo <becauseiami98@gmail.com>
2023-01-04 07:23:30 +0900

    refactor: 프로그래머스 > 코딩테스트 입문

---
+++ src/main/java/org/example/programmers/Lv0_emergency_order.java
@@ -0,0 +1,29 @@
+package org.example.programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Lv0_emergency_order {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(emergency_order(new int[]{3,76,24})));
+        System.out.println(Arrays.toString(emergency_order(new int[]{1,2,3,4,5,6,7})));
+        System.out.println(Arrays.toString(emergency_order(new int[]{30,10,23,6,100})));
+    }
+
+    private static int[] emergency_order(int[] emergency) {
+        int [] sort_arr = emergency.clone(); // Array.sort로 객체 자체가 정렬되기 때문에 clone()으로 복제
+        int[] answer = new int[emergency.length];
+        Arrays.sort(sort_arr);
+        for(int i = 0; i <= emergency.length-1; i++){
+            for(int j = 0; j <= sort_arr.length-1; j++){
+                if(sort_arr[j] == emergency[i]){
+                    answer[j] = emergency.length-i;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+}
